9da4854caa163d7d9e4e24aa4cd5156792d924f1
Andrés Crespo <acrerosj@canariaseducacion.es>
2024-12-11 17:17:38 +0000

    client id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const CLIENT = require('./data/clients.json');
 
 app.get('/', (req, res) => {
   res.send('Hola mundo!!');
@@ -10,10 +11,26 @@ app.get('/api', (req, res) => {
   res.send('api de clientes');
 });
 
+app.get('/api/client', (req, res) => {
+  res.json(CLIENT.map(c => {
+    return {
+      id: c.id,
+      nombre: c.nombre,
+      apellidos: c.apellidos,
+      email: c.cuenta.email
+    }
+  }));
+});
+
 app.get('/api/client/:id', (req, res) => {
   const id = req.params.id;
-  res.send('Dados del cliente con id = ' + id);
-});
+  let filterClient = CLIENT.filter(c => c.id == id);
+  if (filterClient.length) {
+    res.json(filterClient[0]);
+  } else {
+    res.send('El cliente con id = ' + id + ' no encontrado');
+  }
+}); 
 
 app.use(express.static('public'));
 
